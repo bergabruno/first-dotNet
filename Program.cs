365a3e7e84bf0ad05419e5b717fc8dcd8c6d0bc2
bergabruno <brunoberga77@gmail.com>
2024-02-01 00:25:43 -0300

    create main class and add method to get bigger and smaller number in a random sequence

--- Program.cs
+++ Program.cs
@@ -1,4 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-String helloWorld = "Hello World!";
+﻿using System;
 
-Console.WriteLine(helloWorld);
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            helloWorld();
+            biggerSmaller();
+        }
+
+        public static void helloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void biggerSmaller()
+        {
+            Random rnd = new Random();
+
+            int[] numeros = new int[10];
+            int bigger = int.MinValue;
+            int smaller = int.MaxValue;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = rnd.Next();
+                Console.WriteLine(numeros[i]);
+
+                if (numeros[i] > bigger)
+                {
+                    bigger = numeros[i];
+                }
+
+                if (numeros[i] < smaller)
+                {
+                    smaller = numeros[i];
+                }
+            }
+
+            Console.WriteLine(bigger);
+            Console.WriteLine(smaller);
+        }
+    }
+}
